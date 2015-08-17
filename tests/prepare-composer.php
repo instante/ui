6e5d2e8b329e3412b5bd71435d876f13510ca12a
Richard Ejem <richard@ejem.cz>
2015-08-17 15:50:59 +0200

    fixed CS

--- tests/prepare-composer.php
+++ tests/prepare-composer.php
@@ -4,13 +4,13 @@ $rootDir = __DIR__ . '/..';
 $testsDir = __DIR__;
 
 if (getenv('NETTE') !== 'default') {
-	$composerFile = $testsDir . '/composer-' . getenv('NETTE') . '.json';
+    $composerFile = $testsDir . '/composer-' . getenv('NETTE') . '.json';
 
-	unlink($rootDir . '/composer.json');
-	copy($composerFile, $rootDir . '/composer.json');
+    unlink($rootDir . '/composer.json');
+    copy($composerFile, $rootDir . '/composer.json');
 
-	echo "Using tests/", basename($composerFile);
+    echo "Using tests/", basename($composerFile);
 
 } else {
-	echo "Using default composer.json";
+    echo "Using default composer.json";
 }
